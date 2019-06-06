d90c1f8e7d6a3dd413679fd064a5ef4234f72eb5
Sam T <sthompson@digitalpulp.com>
2019-06-05 21:00:44 -0400

    tidy form

--- resources/views/schedule/edit.blade.php
+++ resources/views/schedule/edit.blade.php
@@ -26,32 +26,6 @@
                 <input id="end_date" type="date" name="end_date" value="<?php echo $schedule->end_date != null ? date('Y-m-d', strtotime($schedule->end_date)) : null ?>">
             </div>
 
-            <div class="form-item">
-                <legend>Match Weekday</legend>
-                <fieldset>
-                    <label for="weekday_sunday">Sunday</label>
-                    <input type="radio" id="weekday_sunday" name="weekday" value="sun">
-
-                    <label for="weekday_monday">Monday</label>
-                    <input type="radio" id="weekday_monday" name="weekday" value="mon">
-
-                    <label for="weekday_tuesday">Tuesday</label>
-                    <input type="radio" id="weekday_tuesday" name="weekday" value="tue">
-
-                    <label for="weekday_wednesday">Wednesday</label>
-                    <input type="radio" id="weekday_wednesday" name="weekday" value="wed">
-
-                    <label for="weekday_thursday">Thursday</label>
-                    <input type="radio" id="weekday_thursday" name="weekday" value="thu">
-
-                    <label for="weekday_friday">Friday</label>
-                    <input type="radio" id="weekday_friday" name="weekday" value="fri">
-
-                    <label for="weekday_saturday">Saturday</label>
-                    <input type="radio" id="weekday_saturday" name="weekday" value="sat">
-                </fieldset>
-            </div>
-
             <?php if (!empty($schedule->rounds)): ?>
                 <?php foreach ($schedule->rounds as $index => $round): ?>
                     <a href="/round/<?php echo($round->id); ?>">
@@ -69,7 +43,7 @@
 
             <div class="form-actions">
                 <div class="form-item">
-                    <input id="submit" type="submit" value="Create"/>
+                    <input id="submit" type="submit" value="Update"/>
                 </div>
             </div>
 
