6a29e1bd12a6711896c7bea1b7aa388dc4c222a7
Sam Thompson <samtny@gmail.com>
2019-06-07 16:08:18 -0400

    tidy home

--- resources/views/association/home.blade.php
+++ resources/views/association/home.blade.php
@@ -1,17 +1,24 @@
-@extends('layouts.association')
-
-@section('title', $association->name)
+@extends('layouts.full', ['name' => 'home'])
 
 @section('content')
-    <div class="title m-b-md">
-        <?php echo $association->name; ?>
-    </div>
-    <div class="message">
-        <?php echo $association->name; ?>
-    </div>
-    <div class="links">
-        <a href="{{ route('association.submit.score') }}">Submit Scores</a>
-        <a href="{{ route('association.standings') }}">Standings</a>
-        <a href="{{ route('association.schedule') }}">Schedule</a>
+    @component('page-title')
+        @slot('title')
+            <?php echo $association->name; ?>
+        @endslot
+    @endcomponent
+    <div class="link-buttons">
+        <nav class="association-nav">
+            <ul>
+                <li>
+                    <a class="button" href="{{ route('association.submit.score') }}">Submit Scores</a>
+                </li>
+                <li>
+                    <a class="button" href="{{ route('association.standings') }}">Standings</a>
+                </li>
+                <li>
+                    <a class="button" href="{{ route('association.schedule') }}">Schedule</a>
+                </li>
+            </ul>
+        </nav>
     </div>
 @endsection
