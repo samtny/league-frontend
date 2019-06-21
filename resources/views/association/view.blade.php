f380593cabc6304225e61504ba48113a24ad12dc
Sam T <sthompson@digitalpulp.com>
2019-06-20 20:52:47 -0400

    tidy admin

--- resources/views/association/view.blade.php
+++ resources/views/association/view.blade.php
@@ -10,8 +10,9 @@
     <div class="row">
         <h1 class="col"><?php echo $association->name; ?></h1>
     </div>
-    <div class="links row mb-3">
-        <div class="col">
+    <div class="row">
+        <div class="col-md-6 mb-3">
+            <h2>Administer</h2>
             <div class="list-group">
                 <a class="list-group-item list-group-item-action" href="{{ route('association.edit', ['association' => $association]) }}">Edit Details</a>
                 <a class="list-group-item list-group-item-action" href="{{ route('association.divisions', ['association' => $association]) }}">Divisions</a>
@@ -19,36 +20,42 @@
                 <a class="list-group-item list-group-item-action" href="{{ route('association.venues', ['association' => $association]) }}">Venues</a>
                 <a class="list-group-item list-group-item-action" href="{{ route('association.series', ['association' => $association]) }}">Series</a>
                 <a class="list-group-item list-group-item-action" href="{{ route('association.users', ['association' => $association]) }}">Users</a>
-                <?php if(!($association->resultSubmissions->where('approved', 0)->isEmpty())): ?>
-                <a class="list-group-item list-group-item-action" href="{{ route('result_submissions.list', ['association' => $association]) }}">Score Submissions</a>
-                <?php endif; ?>
+            </div>
+        </div>
+        <div class="col-md-6 mb-3">
+            <div class="row">
+                <div class="col mb-3">
+                    <h2>Score Submissions</h2>
+                    <div class="list-group">
+                    <?php $submissions = $association->resultSubmissions->where('approved', 0); ?>
+                    <?php if(!($submissions->isEmpty())): ?>
+                    <a class="list-group-item list-group-item-action d-flex justify-content-between align-items-center" href="{{ route('result_submissions.list', ['association' => $association]) }}">
+                        Score Submissions
+                        <span class="badge badge-primary badge-pill"><?php echo count($submissions); ?></span>
+                    </a>
+                    <?php else: ?>
+                    There are no score submissions to review.
+                    <?php endif; ?>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col mb-3">
+                    <h2>Active Rounds</h2>
+                    <div class="list-group">
+                    @forelse ($association->activeRounds->sortBy('start_date') as $round)
+                    <a class="list-group-item list-group-item-action" href="{{ route('round.edit', ['schedule' => $round->schedule, 'round' => $round]) }}">
+                        <?php echo !empty($round->series) ? $round->series->name : '[no series]'; ?> - <?php echo $round->name; ?> - <?php echo $round->start_date->format('Y-m-d'); ?>
+                    </a>
+                    @empty
+                    There are no active rounds.
+                    @endforelse
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-    <div class="submissions row mb-3">
-        <div class="col">
-            <h2>Score Submissions</h2>
-            <div class="list-group">
-            <?php if(!($association->resultSubmissions->where('approved', 0)->isEmpty())): ?>
-            <a class="list-group-item list-group-item-action" href="{{ route('result_submissions.list', ['association' => $association]) }}">Score Submissions</a>
-            <?php else: ?>
-            There are no score submissions to review.
-            <?php endif; ?>
-            </div>
-        </div>
-    </div>
-    <div class="rounds row mb-3">
-        <div class="col">
-            <h2>Active Rounds</h2>
-            <div class="list-group">
-            @forelse ($association->activeRounds->sortBy('start_date') as $round)
-            <a class="list-group-item list-group-item-action" href="{{ route('round.edit', ['schedule' => $round->schedule, 'round' => $round]) }}">
-                <?php echo !empty($round->series) ? $round->series->name : '[no series]'; ?> - <?php echo $round->name; ?> - <?php echo $round->start_date->format('Y-m-d'); ?>
-            </a>
-            @empty
-            There are no active rounds.
-            @endforelse
-            </div>
-        </div>
+    <div class="rounds row">
+
     </div>
 @endsection
