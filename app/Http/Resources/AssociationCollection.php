616fe8f48173da6114bde7a0aaf8dbb4fd777d6c
Sam T <samtny@gmail.com>
2019-07-08 11:14:31 -0400

    WIP hash api token

--- app/Http/Resources/AssociationCollection.php
+++ app/Http/Resources/AssociationCollection.php
@@ -14,6 +14,11 @@ class AssociationCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'data' => $this->collection,
+            'links' => [
+                'self' => 'link-value',
+            ],
+        ];
     }
 }
