446e360195ba119f6259290b6b751205d784d84a
Sam T <samtny@gmail.com>
2019-07-08 09:49:02 -0400

    WIP api routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,15 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/test', function (Request $request) {
+
+});
+
+use App\Association;
+use App\Http\Resources\Association as AssociationResource;
+use App\Http\Resources\AssociationCollection as AssociationCollectionResource;
+
+Route::get('/associations', function () {
+    return new AssociationCollectionResource(Association::all());
+});
