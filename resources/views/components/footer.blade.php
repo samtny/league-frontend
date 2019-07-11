de8872c99dbeb8338ff6bb1dd8608afc682f30a3
Sam T <samtny@gmail.com>
2019-07-11 12:59:37 -0400

    footer menu

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,16 @@
 <footer>
+    @section('footer-menu')
+        <nav class="footer-menu">
+            <ul>
+                <li>
+                    <a href="{{ route('about') }}">About</a>
+                </li>
+                <li>
+                    <a href="{{ route('contact') }}">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    @show
     @section('footer-copyright')
         <div class="footer-copyright">
             <span class="copyright">© <?php echo date('Y'); ?></span>&nbsp;<span class="attribution">pinballleague.org</span>
